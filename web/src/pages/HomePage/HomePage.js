2b918f560497ed9cec4790de54b21ee743878b85
Lue <escasinasluezyldominic@gmail.com>
2026-03-11 01:25:12 +0800

    IT342 Phase 1 – User Registration and Login Completed

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+
+
+export default function HomePage() {
+
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>Welcome to the home page! This page is protected and can only be accessed by authenticated users.</p>
+        </div>
+    );
+}
