d65de6fdf7e608d80b407667e4e05e4459134448
Lue <escasinasluezyldominic@gmail.com>
2026-03-31 11:58:24 +0800

    bugfix(mb): - password fields' inputs are now type password - username attribute of LoginRequest changed to email (reason why login didn't work)

--- mobile/app/src/main/java/com/example/vara/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vara
-
-data class LoginRequest(
-    val username: String,
-    val password: String
-)
---
+++ mobile/app/src/main/java/com/example/vara/auth/LoginRequest.kt
@@ -0,0 +1,6 @@
+package com.example.vara.auth
+
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
