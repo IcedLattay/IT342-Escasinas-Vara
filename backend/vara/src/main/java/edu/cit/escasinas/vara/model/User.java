75c47b6cc303b126e6f4bb81ec9421c3e93c1629
Lue <escasinasluezyldominic@gmail.com>
2026-04-06 22:49:10 +0800

    feat(be): - added wallet related attributes to user   - wallet transactions   - wallet

--- backend/vara/src/main/java/edu/cit/escasinas/vara/model/User.java
+++ backend/vara/src/main/java/edu/cit/escasinas/vara/model/User.java
@@ -5,6 +5,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="users")
@@ -48,4 +50,10 @@ public class User {
         this.email = email;
         this.password = password;
     }
+
+    @OneToMany(mappedBy = "owner")
+    public List<WalletTransaction> walletTransactions = new ArrayList<>();
+
+    @OneToOne(mappedBy = "owner", cascade = CascadeType.ALL)
+    public Wallet wallet;
 }
