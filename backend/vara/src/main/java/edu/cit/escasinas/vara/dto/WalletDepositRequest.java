9bf58ec71c3de3ba3ec2ceb9cdc0450be7911843
Lue <escasinasluezyldominic@gmail.com>
2026-04-06 23:07:40 +0800

    feat(be): - added wallet related DTOs   - created the WalletDepositRequest   - created the WalletTransactionRetrievalRequest

--- backend/vara/src/main/java/edu/cit/escasinas/vara/dto/WalletDepositRequest.java
+++ backend/vara/src/main/java/edu/cit/escasinas/vara/dto/WalletDepositRequest.java
@@ -1,4 +1,21 @@
 package edu.cit.escasinas.vara.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import edu.cit.escasinas.vara.enums.PaymentMethod;
+
+import java.math.BigDecimal;
+
 public class WalletDepositRequest {
+    public BigDecimal amount;
+    public PaymentMethod paymentMethod;
+
+    public WalletDepositRequest() {}
+
+    public WalletDepositRequest(
+            BigDecimal amount,
+            PaymentMethod paymentMethod
+    ) {
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+    }
 }
--- backend/vara/src/main/java/edu/cit/escasinas/vara/dto/WalletTransactionRetrievalRequest.java
+++ backend/vara/src/main/java/edu/cit/escasinas/vara/dto/WalletTransactionRetrievalRequest.java
@@ -1,4 +1,16 @@
 package edu.cit.escasinas.vara.dto;
 
 public class WalletTransactionRetrievalRequest {
+    public String externalReferenceId;
+    public Long walletTransactionId;
+
+    public WalletTransactionRetrievalRequest() {}
+
+    public WalletTransactionRetrievalRequest(
+            String externalReferenceId,
+            Long walletTransactionId
+    ) {
+        this.externalReferenceId = externalReferenceId;
+        this.walletTransactionId = walletTransactionId;
+    }
 }
