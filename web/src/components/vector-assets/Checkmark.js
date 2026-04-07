0ea2473fd190afdb7862c3471136d38ac08111a0
Lue <escasinasluezyldominic@gmail.com>
2026-04-07 16:18:39 +0800

    feat(fe): - added a vector asset     - Checkmark

---
+++ web/src/components/vector-assets/Checkmark.js
@@ -0,0 +1,9 @@
+
+
+export default function Checkmark({width=1, height=1}) {
+    return (
+        <svg width={`${width}rem`} height={`${height}rem`} viewBox="0 0 1420 1020" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M84.5 535.932L484.206 935.5L1335.5 84.5" stroke="white" stroke-width="169" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    );
+}
