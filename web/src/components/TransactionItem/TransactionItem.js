0f94da5beca14d36dac6a4bb7e63d2978e28a4e8
Lue <escasinasluezyldominic@gmail.com>
2026-04-09 19:41:31 +0800

    chore(fe): - refactored transaction item component's code for readability

--- web/src/components/TransactionItem/TransactionItem.js
+++ web/src/components/TransactionItem/TransactionItem.js
@@ -1,9 +1,19 @@
 
 import "./TransactionItem.css"
 
-export default function TransactionItem({ direction, amount, reason, date, isLast=false }) {
+export default function TransactionItem({ 
+    onClick,
+    transactionId,
+    direction, 
+    amount, 
+    reason, 
+    date, 
+    isLast=false 
+}) {
     return (
-        <div className="transaction-item"
+        <div 
+            key={transactionId}
+            className="transaction-item"
             style={{
                 borderBottom: isLast ? "none" : "1px solid #C3C3C3"
             }}
@@ -12,7 +22,12 @@ export default function TransactionItem({ direction, amount, reason, date, isLas
 
                 <div className="payment transaction">
                     <p>{ direction }</p>
-                    <p style={{fontSize: "1.1rem", marginTop: ".5rem"}}>{ direction=="Debit" ? "- Php" : "Php" } {amount}</p>
+                    <p 
+                        style={{
+                            fontSize: "1.1rem", 
+                            marginTop: ".5rem"
+                        }}
+                    >{ direction=="Debit" ? "- Php" : "Php" } {amount}</p>
                     <p style={{color: "#616161"}}>{reason}</p>
                 </div>
                 
