d02852932562eb9fb879dfb9fed9c8afc5812ed7
Lue <escasinasluezyldominic@gmail.com>
2026-04-13 17:16:19 +0800

    feat(be): - added id in TransactionResponse

--- backend/vara/src/main/java/edu/cit/escasinas/vara/dto/TransactionResponse.java
+++ backend/vara/src/main/java/edu/cit/escasinas/vara/dto/TransactionResponse.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class TransactionResponse {
+    public Long id;
     public String category;
     public String activity;
     public String amount;
